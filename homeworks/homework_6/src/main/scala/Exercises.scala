3449fea76c002d51f94e751a8abf2507cae3bd5e
KoTBegemoTT <56973701+KoTBegemoTT@users.noreply.github.com>
2020-12-16 12:08:19 +0500

    Update Exercises.scala

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,9 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    seq.reverse
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +11,24 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    if (idx > 1)
+      return fibonacci4Index(idx-1) + fibonacci4Index(idx - 2)
+    if (idx == 1)
+      return 1
+    0
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    if (idx == 0)
+      return Seq[Int](0)
+
+    var fibSeq = Seq[Int](0, 1)
+    for (i <- 2 to idx) {
+      fibSeq :+= fibSeq.apply(i-1) + fibSeq.apply(i-2)
+    }
+    fibSeq
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +36,26 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def getMorzeCod(symbol: Char): String ={
+    if (symbol == ' ')
+      return "  "
+    if (MORSE.contains(symbol.toString))
+      return " " + MORSE(symbol.toString)
+    symbol.toString()
+  }
+  def morse(text: String): String = {
+    val mText = text.toUpperCase.map(s => getMorzeCod(s))
+    if (MORSE.contains(text(0).toString))
+      mText.mkString("").replaceFirst(" ", "")
+    else mText.mkString("")
+  }
 
-
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = {
+    text.split("(?=[!. ,?])|(?<=[ ])")
+      .map(w =>
+        if (w(0).isUpper)
+          w.toLowerCase.reverse.capitalize
+        else w.reverse).mkString("")
+  }
 
 }
